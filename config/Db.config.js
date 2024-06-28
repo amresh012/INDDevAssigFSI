cb384474d4f191f217a3cd9a2dc628765de66d14
amresh012 <73485115+amresh012@users.noreply.github.com>
2024-06-28 18:09:17 +0530

    add logger

--- config/Db.config.js
+++ config/Db.config.js
@@ -6,7 +6,7 @@ dotenv.config();
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI)
-    console.log(`MongoDB connected to ${conn.connection.host}`);
+    console.log(`Sucessfully Connected to MongoDB `);
   } catch (err) {
     console.error(`Error connecting to MongoDB: ${err.message}`);
     process.exit(1);
